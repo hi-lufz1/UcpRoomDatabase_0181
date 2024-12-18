90f4ce5079437186e8f3ce0efd82cd0b7d25ab5b
hi-lufz1 <latifufauzi@gmail.com>
2024-12-18 14:29:09 +0700

    membuat local repository Barang

---
+++ app/src/main/java/com/example/ucproomdb/data/repository/LocalRepoBrg.kt
@@ -0,0 +1,31 @@
+package com.example.ucproomdb.data.repository
+
+import com.example.ucproomdb.data.dao.BarangDao
+import com.example.ucproomdb.data.entity.Barang
+import kotlinx.coroutines.flow.Flow
+
+class LocalRepoBrg(
+    private val barangDao: BarangDao
+) : RepositoryBrg {
+
+    override suspend fun insertBrg(barang: Barang) {
+        barangDao.insertBarang(barang)
+    }
+
+    override fun getAllBrg(): Flow<List<Barang>> {
+       return barangDao.getAllBarang()
+    }
+
+    override fun getBrg(idBarang: String): Flow<Barang> {
+       return barangDao.getBarang(idBarang)
+    }
+
+    override suspend fun deleteBrg(barang: Barang) {
+        barangDao.deleteBarang(barang)
+    }
+
+    override suspend fun updateMhs(barang: Barang) {
+       barangDao.updateBarang(barang)
+    }
+
+}
