fe5d4c0e48aa6489250717e14a6241675c9518ec
hi-lufz1 <latifufauzi@gmail.com>
2024-12-20 00:36:28 +0700

    membuat data class untuk validasi input barang

--- app/src/main/java/com/example/ucproomdb/ui/viewmodel/InsertBrgViewModel.kt
+++ app/src/main/java/com/example/ucproomdb/ui/viewmodel/InsertBrgViewModel.kt
@@ -2,6 +2,21 @@ package com.example.ucproomdb.ui.viewmodel
 
 import com.example.ucproomdb.data.entity.Barang
 
+data class FormErrorState(
+    val idBarang :String? = null,
+    val nama :String? = null,
+    val deskripsi :String? = null,
+    val harga :Int? = null,
+    val stok :Int? = null,
+    val namaSupplier :String? = null,
+) {
+    fun isValid(): Boolean {
+        return idBarang == null && nama == null && deskripsi == null &&
+                harga == null && stok == null && namaSupplier == null
+    }
+}
+
+
 
 fun BarangEvent.toBaranEntity(): Barang = Barang(
     idBarang = idBarang,
