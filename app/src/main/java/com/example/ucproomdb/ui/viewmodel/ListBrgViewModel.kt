03dc62eff04e99ecf3485a9378e78057a71b2451
hi-lufz1 <latifufauzi@gmail.com>
2024-12-19 23:21:33 +0700

    membuat view model list barang

--- app/src/main/java/com/example/ucproomdb/ui/viewmodel/ListBrgViewModel.kt
+++ app/src/main/java/com/example/ucproomdb/ui/viewmodel/ListBrgViewModel.kt
@@ -1,9 +1,51 @@
 package com.example.ucproomdb.ui.viewmodel
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucproomdb.data.entity.Barang
+import com.example.ucproomdb.data.repository.RepositoryBrg
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 
-
-
+class ListBrgViewModel(
+    private val
+    repositoryBrg: RepositoryBrg
+) : ViewModel() {
+    val homeUiState: StateFlow<BarangUiState> = repositoryBrg.getAllBrg()
+        .filterNotNull()
+        .map {
+            BarangUiState(
+                listBrg = it.toList(),
+                isLoading = false
+            )
+        }
+        .onStart {
+            emit(BarangUiState(isLoading = true))
+            delay(500)
+        }
+        .catch {
+            emit(
+                BarangUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Error"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = BarangUiState(
+                isLoading = true
+            )
+        )
+}
 
 
 data class BarangUiState(
