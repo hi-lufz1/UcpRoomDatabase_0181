640f1bda9ab82f9637390e25456958d75823e1ff
hi-lufz1 <latifufauzi@gmail.com>
2024-12-20 16:10:32 +0700

    membuat PenyediaViewModel

---
+++ app/src/main/java/com/example/ucproomdb/ui/viewmodel/PenyediaViewModel.kt
@@ -0,0 +1,26 @@
+package com.example.ucproomdb.ui.viewmodel
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.ucproomdb.InventoryApp
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            InsertBrgViewModel(
+                inventoryApp().containerApp.repositoryBrg,
+                inventoryApp().containerApp.repositorySupp
+            )
+        }
+        initializer {
+            InsertSuppViewModel(
+                inventoryApp().containerApp.repositorySupp
+            )
+        }
+    }
+}
+
+fun CreationExtras.inventoryApp(): InventoryApp =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as InventoryApp)
